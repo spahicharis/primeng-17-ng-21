1552c62fe57ba3dea222d17f40b9685f86e0ccf7
Haris Spahić <spahaa@hotmail.com>
2026-09-04 10:22:32 +0200

    wip

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,19 @@
-import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
-import { AppComponent } from 'src/app/showcase/layout/app.component';
-import { config } from 'src/app/showcase/layout/app.config.server';
+import { provideZoneChangeDetection } from "@angular/core";
+import {
+  bootstrapApplication,
+  BootstrapContext,
+} from "@angular/platform-browser";
+import { AppComponent } from "src/app/showcase/layout/app.component";
+import { config } from "src/app/showcase/layout/app.config.server";
 
-const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
+const bootstrap = (context: BootstrapContext) =>
+  bootstrapApplication(
+    AppComponent,
+    {
+      ...config,
+      providers: [provideZoneChangeDetection(), ...config.providers],
+    },
+    context,
+  );
 
 export default bootstrap;
